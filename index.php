794073b09f49486d7fff02e156d46d2e7b7cc925
Elijah Nathan <ely.nathan93@gmail.com>
2017-10-07 09:59:21 +0100

    Altered comments to be across multiple lines

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-// Is this comment here? added added2
+/*
+1: Is this comment here?
+2: added
+3: added2
+*/
 $error = "";
 $success = "";
 if (isset($_POST['contactSub']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
